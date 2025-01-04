85040c2749c8fe651037315e7ddfb642fe0992d2
TEJAS RAJENDRA THONGE <80546718+tejasthonge@users.noreply.github.com>
2025-01-04 08:43:35 +0530

    by using this we get the current app envermet or set the envarment

--- 5_Express_advanced/5_environments/app.js
+++ 5_Express_advanced/5_environments/app.js
@@ -1,7 +1,7 @@
 
 
 const express = require('express');
-
+const morgen = require('morgan');
 const app = express();
 console.log(`Node Current Environment: ${process.env.NODE_ENV}`);
 console.log(`App Environment: ${app.get('env')}`);
@@ -10,7 +10,7 @@ console.log(`App Environment: ${app.get('env')}`);
 op:
 Node Current Environment: undefined
 App Environment: development     
-*/ 
+*/
 
 // the above output is due to
 //we are not NODE_ENV 
@@ -19,6 +19,37 @@ App Environment: development
 //we set NODE_ENV as 
 //export NODE_ENV=<your envornmet>
 
-if(app.get('env'==="development")){
+
+if (app.get('env') === "development") {
+    console.log("development");
     app.use(morgen('tiny'))
 }
+else if (app.get('env') === "production") {
+    console.log("production");
+
+}
+
+
+
+app.listen(3000, () => console.log("server is lissinging at 3000"))
+
+/*
+ >_ export NODE_ENV=production 
+~/coding/github/NodeJS-Practice-Code/5_Express_advanced/5_environments >_ nodemon app.js            
+[nodemon] 3.1.9
+[nodemon] to restart at any time, enter `rs`
+[nodemon] watching path(s): *.*
+[nodemon] watching extensions: js,mjs,cjs,json
+[nodemon] starting `node app.js`
+Node Current Environment: production
+App Environment: production
+production
+server is lissinging at 3000
+[nodemon] restarting due to changes...
+[nodemon] starting `node app.js`
+Node Current Environment: production
+App Environment: production
+production
+server is lissinging at 3000
+
+*/
